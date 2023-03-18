593a375f2f417bfd7f841a2dbc9f47d69f6e7460
hirokazu.kobayashi <hirokazu.kobayashi.koba.hiro@gmail.com>
2023-03-18 22:25:11 +0900

    improve multi project

--- app/src/main/java/org/idp/sample/SampleApplication.java
+++ app/src/main/java/org/idp/sample/SampleApplication.java
@@ -1,13 +1,24 @@
 package org.idp.sample;
 
+import org.idp.server.IdpServerApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class SampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SampleApplication.class, args);
+    }
+
+    @Bean
+    public IdpServerApplication idpServerApplication() {
+        List<String> paths = new ArrayList<>();
+        return IdpServerApplication.initializeWithInMemory(paths, paths);
     }
 
 }
