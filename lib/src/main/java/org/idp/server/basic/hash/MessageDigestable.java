6d2632a2d6384b30c817790e662781173535541c
hirokazu.kobayashi <hirokazu.kobayashi.koba.hiro@gmail.com>
2023-03-27 07:53:14 +0900

    create MessageDigestable2

--- lib/src/main/java/org/idp/server/basic/hash/MessageDigestable.java
+++ lib/src/main/java/org/idp/server/basic/hash/MessageDigestable.java
@@ -1,37 +1,30 @@
 package org.idp.server.basic.hash;
 
-import org.apache.commons.codec.binary.Hex;
-
 import java.security.MessageDigest;
 public interface MessageDigestable {
 
-    default String digestWithMD5(String value) {
+    default byte[] digestWithMD5(String value) {
         MessageDigest messageDigest = HashAlgorithm.MD5.messageDigest();
-        byte[] digest = messageDigest.digest(value.getBytes());
-        return Hex.encodeHexString(digest);
+        return messageDigest.digest(value.getBytes());
     }
-    default String digestWithSha1(String value) {
+    default byte[] digestWithSha1(String value) {
         MessageDigest messageDigest = HashAlgorithm.SHA_1.messageDigest();
-        byte[] digest = messageDigest.digest(value.getBytes());
-        return Hex.encodeHexString(digest);
+        return messageDigest.digest(value.getBytes());
     }
 
-    default String digestWithSha256(String value) {
+    default byte[] digestWithSha256(String value) {
         MessageDigest messageDigest = HashAlgorithm.SHA_256.messageDigest();
-        byte[] digest = messageDigest.digest(value.getBytes());
-        return Hex.encodeHexString(digest);
+        return messageDigest.digest(value.getBytes());
     }
 
-    default String digestWithSha384(String value) {
+    default byte[] digestWithSha384(String value) {
         MessageDigest messageDigest = HashAlgorithm.SHA_384.messageDigest();
-        byte[] digest = messageDigest.digest(value.getBytes());
-        return Hex.encodeHexString(digest);
+        return messageDigest.digest(value.getBytes());
     }
 
-    default String digestWithSha512(String value) {
+    default byte[] digestWithSha512(String value) {
         MessageDigest messageDigest = HashAlgorithm.SHA_512.messageDigest();
-        byte[] digest = messageDigest.digest(value.getBytes());
-        return Hex.encodeHexString(digest);
+        return messageDigest.digest(value.getBytes());
     }
 
 }
