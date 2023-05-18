1dee00229f832badbbc6b203be411da85922ece1
hirokazu.kobayashi <hirokazu.kobayashi.koba.hiro@gmail.com>
2023-05-18 19:56:17 +0900

    add e2w of request object case

--- e2e/src/oidc_core_6_request_object.test.js
+++ e2e/src/oidc_core_6_request_object.test.js
@@ -1,9 +1,9 @@
-import { describe, expect, it } from "@jest/globals";
+import { describe, expect, it, xit } from "@jest/globals";
 
 import { getJwks, requestToken } from "./api/oauthClient";
 import { clientSecretPostClient, serverConfig } from "./testConfig";
 import { requestAuthorizations } from "./oauth";
-import { createJwtWithPrivateKey, generateJti, verifyAndDecodeIdToken } from "./lib/jose";
+import { createJwt, createJwtWithPrivateKey, generateJti, verifyAndDecodeIdToken } from "./lib/jose";
 import { toEpocTime } from "./lib/util";
 
 describe("OpenID Connect Core 1.0 incorporating errata set 1 request object", () => {
@@ -54,5 +54,155 @@ describe("OpenID Connect Core 1.0 incorporating errata set 1 request object", ()
       jwks: jwksResponse.data,
     });
     console.log(decodedIdToken);
+  });
+
+  describe("6.  Passing Request Parameters as JWTs", () => {
+    it("The JWT MUST be secured with an asymmetric signature and follow the guidance from Section 10.1 of [OpenID.Core] regarding asymmetric signatures. ", async () => {
+      const request = createJwt({
+        payload: {
+          client_id: clientSecretPostClient.clientId,
+          response_type: "code",
+          state: "aiueo",
+          scope: "openid profile phone email " + clientSecretPostClient.scope,
+          redirect_uri: clientSecretPostClient.redirectUri,
+          aud: "aud",
+          iss: clientSecretPostClient.clientId,
+        },
+        secret: clientSecretPostClient.clientSecret,
+      });
+      const { authorizationResponse } = await requestAuthorizations({
+        endpoint: serverConfig.authorizationEndpoint,
+        request,
+        clientId: clientSecretPostClient.clientId,
+      });
+      console.log(authorizationResponse);
+      console.log(authorizationResponse.data);
+      expect(authorizationResponse.error).toEqual(
+        "invalid_request_object"
+      );
+      expect(
+        authorizationResponse.errorDescription
+      ).toEqual(
+        "request object is invalid, request object must signed with asymmetric key"
+      );
+    });
+
+    it("aud The Audience claim MUST contain the value of the Issuer Identifier for the OP, which identifies the Authorization Server as an intended audience.", async () => {
+      const request = createJwtWithPrivateKey({
+        payload: {
+          client_id: clientSecretPostClient.clientId,
+          response_type: "code",
+          state: "aiueo",
+          scope: "openid profile phone email " + clientSecretPostClient.scope,
+          redirect_uri: clientSecretPostClient.redirectUri,
+          aud: "aud",
+          iss: clientSecretPostClient.clientId,
+        },
+        privateKey: clientSecretPostClient.requestKey,
+      });
+      const { authorizationResponse } = await requestAuthorizations({
+        endpoint: serverConfig.authorizationEndpoint,
+        request,
+        clientId: clientSecretPostClient.clientId,
+      });
+      console.log(authorizationResponse);
+      console.log(authorizationResponse.data);
+      expect(authorizationResponse.error).toEqual(
+        "invalid_request_object"
+      );
+      expect(
+        authorizationResponse.errorDescription
+      ).toEqual("request object is invalid, aud claim must be issuer");
+    });
+
+    it("iss The Issuer claim MUST be the client_id of the OAuth Client.", async () => {
+      const request = createJwtWithPrivateKey({
+        payload: {
+          client_id: clientSecretPostClient.clientId,
+          response_type: "code",
+          state: "aiueo",
+          scope: "openid profile phone email " + clientSecretPostClient.scope,
+          redirect_uri: clientSecretPostClient.redirectUri,
+          aud: serverConfig.issuer,
+          iss: "clientId",
+        },
+        privateKey: clientSecretPostClient.requestKey,
+      });
+      const { authorizationResponse } = await requestAuthorizations({
+        endpoint: serverConfig.authorizationEndpoint,
+        request,
+        clientId: clientSecretPostClient.clientId,
+      });
+      console.log(authorizationResponse);
+      console.log(authorizationResponse.data);
+      expect(authorizationResponse.error).toEqual(
+        "invalid_request_object"
+      );
+      expect(
+        authorizationResponse.errorDescription
+      ).toEqual("request object is invalid, iss claim must be client_id");
+    });
+
+    it("exp An expiration time that limits the validity lifetime of the signed authentication request.", async () => {
+      const request = createJwtWithPrivateKey({
+        payload: {
+          client_id: clientSecretPostClient.clientId,
+          response_type: "code",
+          state: "aiueo",
+          scope: "openid profile phone email " + clientSecretPostClient.scope,
+          redirect_uri: clientSecretPostClient.redirectUri,
+          aud: serverConfig.issuer,
+          iss: clientSecretPostClient.clientId,
+          exp: toEpocTime({ plus: -10 }),
+          iat: toEpocTime({}),
+          nbf: toEpocTime({}),
+          jti: generateJti(),
+        },
+        privateKey: clientSecretPostClient.requestKey,
+      });
+
+      const { authorizationResponse } = await requestAuthorizations({
+        endpoint: serverConfig.authorizationEndpoint,
+        request,
+        clientId: clientSecretPostClient.clientId,
+      });
+      console.log(authorizationResponse);
+      console.log(authorizationResponse.data);
+      expect(authorizationResponse.error).toEqual(
+        "invalid_request_object"
+      );
+      expect(
+        authorizationResponse.errorDescription
+      ).toEqual("request object is invalid, jwt is expired");
+    });
+
+    xit("iat The time at which the signed authentication request was created.", async () => {
+      const request = createJwtWithPrivateKey({
+        payload: {
+          client_id: clientSecretPostClient.clientId,
+          response_type: "code",
+          state: "aiueo",
+          scope: "openid profile phone email " + clientSecretPostClient.scope,
+          redirect_uri: clientSecretPostClient.redirectUri,
+          aud: serverConfig.issuer,
+          iss: clientSecretPostClient.clientId,
+          exp: toEpocTime({ plus: 3000 }),
+          iat: toEpocTime({ plus: 1000 }),
+          nbf: toEpocTime({}),
+          jti: generateJti(),
+        },
+        privateKey: clientSecretPostClient.requestKey,
+      });
+      const { authorizationResponse } = await requestAuthorizations({
+        endpoint: serverConfig.authorizationEndpoint,
+        request,
+        clientId: clientSecretPostClient.clientId,
+      });
+      console.log(authorizationResponse);
+      console.log(authorizationResponse.data);
+      expect(authorizationResponse.error).toEqual(
+        "invalid_request_object"
+      );
+    });
   });
 });
