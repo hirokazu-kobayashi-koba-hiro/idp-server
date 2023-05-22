89545061fdbad34bb470a98d337798a70fd141e2
hirokazu.kobayashi <hirokazu.kobayashi.koba.hiro@gmail.com>
2023-05-23 08:05:22 +0900

    remove unused importing function

--- e2e/src/oidc_core_2_id_token.test.js
+++ e2e/src/oidc_core_2_id_token.test.js
@@ -1,9 +1,9 @@
 import { describe, expect, it } from "@jest/globals";
 
-import { getJwks, getUserinfo, postUserinfo, requestToken } from "./api/oauthClient";
+import { getJwks } from "./api/oauthClient";
 import { clientSecretPostClient, serverConfig } from "./testConfig";
 import { requestAuthorizations } from "./oauth";
-import { createBearerHeader, isNumber, matchWithUSASCII } from "./lib/util";
+import { isArray, isNumber, isString, matchWithUSASCII } from "./lib/util";
 import { verifyAndDecodeIdToken } from "./lib/jose";
 
 describe("OpenID Connect Core 1.0 incorporating errata set 1 id_token", () => {
@@ -259,7 +259,6 @@ describe("OpenID Connect Core 1.0 incorporating errata set 1 id_token", () => {
       expect(payload.nonce).toEqual(nonce);
     });
 
-
     it("acr OPTIONAL. Authentication Context Class Reference. String specifying an Authentication Context Class Reference value that identifies the Authentication Context Class that the authentication performed satisfied. The value \"0\" indicates the End-User authentication did not meet the requirements of ISO/IEC 29115 [ISO29115] level 1. Authentication using a long-lived browser cookie, for instance, is one example where the use of \"level 0\" is appropriate. Authentications with level 0 SHOULD NOT be used to authorize access to any resource of any monetary value. (This corresponds to the OpenID 2.0 PAPE [OpenID.PAPE] nist_auth_level 0.) An absolute URI or an RFC 6711 [RFC6711] registered name SHOULD be used as the acr value; registered names MUST NOT be used with a different meaning than that which is registered. Parties using this claim will need to agree upon the meanings of the values used, which may be context-specific. The acr value is a case sensitive string.", async () => {
       const nonce = "nonce_value";
       const { authorizationResponse } = await requestAuthorizations({
@@ -303,7 +302,59 @@ describe("OpenID Connect Core 1.0 incorporating errata set 1 id_token", () => {
       expect(payload.nonce).not.toBeNull();
       expect(payload.nonce).toEqual(nonce);
 
-      //TODO acr
+      if (payload.acr) {
+        expect(isString(payload.acr)).toBe(true);
+      }
+    });
+
+    it("amr OPTIONAL. Authentication Methods References. JSON array of strings that are identifiers for authentication methods used in the authentication. For instance, values might indicate that both password and OTP authentication methods were used. The definition of particular values to be used in the amr Claim is beyond the scope of this specification. Parties using this claim will need to agree upon the meanings of the values used, which may be context-specific. The amr value is an array of case sensitive strings.", async () => {
+      const nonce = "nonce_value";
+      const { authorizationResponse } = await requestAuthorizations({
+        endpoint: serverConfig.authorizationEndpoint,
+        clientId: clientSecretPostClient.clientId,
+        responseType: "id_token",
+        state: "aiueo",
+        nonce,
+        scope: "openid profile phone email" + clientSecretPostClient.scope,
+        redirectUri: clientSecretPostClient.redirectUri,
+      });
+      console.log(authorizationResponse);
+      expect(authorizationResponse.idToken).not.toBeNull();
+
+      const jwksResponse = await getJwks({ endpoint: serverConfig.jwksEndpoint });
+      console.log(jwksResponse.data);
+      expect(jwksResponse.status).toBe(200);
+
+      const decodedIdToken = verifyAndDecodeIdToken({
+        idToken: authorizationResponse.idToken,
+        jwks: jwksResponse.data,
+      });
+      console.log(decodedIdToken);
+      const payload = decodedIdToken.payload;
+      expect(decodedIdToken.verifyResult).toBe(true);
+      expect(payload.iss).not.toBeNull();
+      expect(payload.iss).toEqual(serverConfig.issuer);
+      expect(payload.sub).not.toBeNull();
+      expect(payload.sub.length <= 255).toBe(true);
+      expect(matchWithUSASCII(payload.sub)).toEqual(true);
+      expect(payload.aud).not.toBeNull();
+      expect(payload.aud).toContain(clientSecretPostClient.clientId);
+      expect(payload.exp).not.toBeNull();
+      expect(isNumber(payload.exp)).toBe(true);
+      expect(payload.iat).not.toBeNull();
+      expect(isNumber(payload.iat)).toBe(true);
+      if (payload.auth_time) {
+        expect(isNumber(payload.auth_time)).toBe(true);
+      }
+      expect(payload.nonce).not.toBeNull();
+      expect(payload.nonce).toEqual(nonce);
+      if (payload.acr) {
+        expect(isString(payload.acr)).toBe(true);
+      }
+
+      if (payload.amr) {
+        expect(isArray(payload.amr)).toBe(true);
+      }
     });
   });
 });
