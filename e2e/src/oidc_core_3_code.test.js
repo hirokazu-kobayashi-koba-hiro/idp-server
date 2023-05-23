e36ac0586ec139ccb8dade0df287d4584d05ce67
hirokazu.kobayashi <hirokazu.kobayashi.koba.hiro@gmail.com>
2023-05-24 08:04:44 +0900

    add e2e test case in oidc_core_3_code.test.js

--- e2e/src/oidc_core_3_code.test.js
+++ e2e/src/oidc_core_3_code.test.js
@@ -304,4 +304,85 @@ describe("OpenID Connect Core 1.0 incorporating errata set 1 code", () => {
       );
     });
   });
+
+  describe("3.1.2.2.  Authentication Request Validation", () => {
+    //The Authorization Server MUST validate the request received as follows:
+    it("1. The Authorization Server MUST validate all the OAuth 2.0 parameters according to the OAuth 2.0 specification.", async () => {
+      const { status, authorizationResponse } = await requestAuthorizations({
+        endpoint: serverConfig.authorizationEndpoint,
+        clientId: clientSecretPostClient.clientId,
+        redirectUri: clientSecretPostClient.redirectUri,
+        scope: "openid " + clientSecretPostClient.scope,
+        state: "state",
+        responseMode: "query",
+        nonce: "nonce",
+        display: "page",
+        maxAge: "-100",
+      });
+      console.log(authorizationResponse);
+      expect(status).toBe(302);
+
+      expect(authorizationResponse.error).toEqual("invalid_request");
+      expect(authorizationResponse.errorDescription).toEqual(
+        "response type is required in authorization request"
+      );
+    });
+
+    it("2. Verify that a scope parameter is present and contains the openid scope value. (If no openid scope value is present, the request may still be a valid OAuth 2.0 request, but is not an OpenID Connect request.)", async () => {
+      const { status, error } = await requestAuthorizations({
+        endpoint: serverConfig.authorizationEndpoint,
+        clientId: clientSecretPostClient.clientId,
+        redirectUri: "https://client.example.org:443/callback",
+        responseType: "code",
+        scope: "openid " + clientSecretPostClient.scope,
+        state: "state",
+        responseMode: "query",
+        nonce: "nonce",
+        display: "page",
+      });
+      console.log(error);
+      expect(status).toBe(400);
+
+      expect(error.error).toEqual("invalid_request");
+      expect(error.error_description).toEqual(
+        "authorization request redirect_uri does not register in client configuration (https://client.example.org:443/callback)"
+      );
+    });
+
+    it("3. The Authorization Server MUST verify that all the REQUIRED parameters are present and their usage conforms to this specification.", async () => {
+      const { status, authorizationResponse } = await requestAuthorizations({
+        endpoint: serverConfig.authorizationEndpoint,
+        clientId: clientSecretPostClient.clientId,
+        redirectUri: clientSecretPostClient.redirectUri,
+        responseType: "code",
+        state: "state",
+        responseMode: "query",
+        nonce: "nonce",
+        display: "page",
+      });
+      console.log(authorizationResponse);
+      expect(status).toBe(302);
+
+      expect(authorizationResponse.error).toEqual("invalid_scope");
+      expect(authorizationResponse.errorDescription).toEqual(
+        "authorization request does not contains valid scope ()"
+      );
+    });
+
+    it("4. If the sub (subject) Claim is requested with a specific value for the ID Token, the Authorization Server MUST only send a positive response if the End-User identified by that sub value has an active session with the Authorization Server or has been Authenticated as a result of the request. The Authorization Server MUST NOT reply with an ID Token or Access Token for a different user, even if they have an active session with the Authorization Server. Such a request can be made either using an id_token_hint parameter or by requesting a specific Claim Value as described in Section 5.5.1, if the claims parameter is supported by the implementation.", async () => {
+      const { authorizationResponse } = await requestAuthorizations({
+        endpoint: serverConfig.authorizationEndpoint,
+        clientId: clientSecretPostClient.clientId,
+        redirectUri: clientSecretPostClient.redirectUri,
+        responseType: "code",
+        scope: "openid " + clientSecretPostClient.scope,
+        state: "state",
+        responseMode: "query",
+        nonce: "nonce",
+        display: "page",
+      });
+      console.log(authorizationResponse);
+      expect(authorizationResponse.code).not.toBeNull();
+    });
+  });
 });
