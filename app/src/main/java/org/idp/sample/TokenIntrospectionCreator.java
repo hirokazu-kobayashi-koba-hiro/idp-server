473b1e251ae408f90e29fb055b41acf01e04dddf
hirokazu.kobayashi <hirokazu.kobayashi.koba.hiro@gmail.com>
2025-02-06 08:54:47 +0900

    implement SampleApplicationFilter.java in order to handle bearer token.

---
+++ app/src/main/java/org/idp/sample/TokenIntrospectionCreator.java
@@ -0,0 +1,24 @@
+package org.idp.sample;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.idp.server.handler.tokenintrospection.io.TokenIntrospectionRequest;
+import org.idp.server.token.AuthorizationHeaderHandlerable;
+
+public class TokenIntrospectionCreator implements AuthorizationHeaderHandlerable {
+
+  String authorizationHeader;
+  String issuer;
+
+  public TokenIntrospectionCreator(String authorizationHeader, String issuer) {
+    this.authorizationHeader = authorizationHeader;
+    this.issuer = issuer;
+  }
+
+  public TokenIntrospectionRequest create() {
+    var accessToken = extractAccessToken(authorizationHeader);
+    Map<String, String[]> map = new HashMap<>();
+    map.put("token", new String[] {accessToken.value()});
+    return new TokenIntrospectionRequest(map, issuer);
+  }
+}
