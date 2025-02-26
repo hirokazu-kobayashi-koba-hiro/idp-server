83fd646b830ac4d6e227e7473858e2e6bf689761
hirokazu.kobayashi <hirokazu.kobayashi.koba.hiro@gmail.com>
2025-02-26 23:45:38 +0900

    change cors

--- app/src/main/java/org/idp/sample/SecurityConfig.java
+++ app/src/main/java/org/idp/sample/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
   public UrlBasedCorsConfigurationSource corsConfigurationSource() {
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     CorsConfiguration config = new CorsConfiguration();
-    config.setAllowedOrigins(List.of("http://localhost:3100"));
+    config.setAllowedOrigins(List.of(authViewUrl, "http://localhost:3100"));
     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     config.setAllowedHeaders(List.of("*"));
     config.setAllowCredentials(true);
