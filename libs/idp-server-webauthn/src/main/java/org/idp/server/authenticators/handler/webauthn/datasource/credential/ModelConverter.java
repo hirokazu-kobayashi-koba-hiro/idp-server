3cc0a37ad9d2ccca2769585fb61c7ae6b9e10f33
hirokazu.kobayashi <hirokazu.kobayashi.koba.hiro@gmail.com>
2025-03-16 16:17:10 +0900

    change method to "HexFormat.of().parseHex"

--- libs/idp-server-webauthn/src/main/java/org/idp/server/authenticators/handler/webauthn/datasource/credential/ModelConverter.java
+++ libs/idp-server-webauthn/src/main/java/org/idp/server/authenticators/handler/webauthn/datasource/credential/ModelConverter.java
@@ -3,12 +3,14 @@ package org.idp.server.authenticators.handler.webauthn.datasource.credential;
 import org.idp.server.authenticators.webauthn.WebAuthnCredential;
 
 import java.util.Base64;
+import java.util.HexFormat;
 import java.util.Map;
 
 class ModelConverter {
 
   static WebAuthnCredential convert(Map<String, String> stringMap) {
-    byte[] id = Base64.getDecoder().decode(stringMap.get("id"));
+
+    byte[] id = HexFormat.of().parseHex(stringMap.get("id"));
     String userId = stringMap.get("user_id");
     String rpId = stringMap.get("rp_id");
     byte[] publicKey = Base64.getDecoder().decode(stringMap.get("public_key"));
