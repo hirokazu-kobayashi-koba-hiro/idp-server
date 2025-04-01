213cc1fc39e8806091c2a1a885fe542a71961baa
hirokazu.kobayashi <hirokazu.kobayashi.koba.hiro@gmail.com>
2025-04-01 23:15:29 +0900

    remove unused library

--- libs/idp-server-springboot-adapter/src/main/java/org/idp/server/adapters/springboot/restapi/admin/AdminRegistrationV1Api.java
+++ libs/idp-server-springboot-adapter/src/main/java/org/idp/server/adapters/springboot/restapi/admin/AdminRegistrationV1Api.java
@@ -6,7 +6,6 @@ import org.idp.server.core.api.IdpServerStarterApi;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +15,6 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/admin/registration")
-@Transactional
 public class AdminRegistrationV1Api implements ParameterTransformable {
 
   IdpServerStarterApi idpServerStarterApi;
--- libs/idp-server-springboot-adapter/src/main/java/org/idp/server/adapters/springboot/restapi/admin/ServerRegistrationV1Api.java
+++ libs/idp-server-springboot-adapter/src/main/java/org/idp/server/adapters/springboot/restapi/admin/ServerRegistrationV1Api.java
@@ -8,12 +8,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
+
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/admin/server/registration")
-@Transactional
 public class ServerRegistrationV1Api implements ParameterTransformable {
 
   ServerDomain serverDomain;
