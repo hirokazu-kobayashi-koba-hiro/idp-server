c31fdfc4ac4223150e39867366da279e9acbe884
hirokazu.kobayashi <hirokazu.kobayashi.koba.hiro@gmail.com>
2025-05-06 16:19:45 +0900

    debug

--- libs/idp-server-core/src/main/java/org/idp/server/core/oidc/vp/request/InputDescriptorConstrains.java
+++ libs/idp-server-core/src/main/java/org/idp/server/core/oidc/vp/request/InputDescriptorConstrains.java
@@ -4,8 +4,7 @@ import java.util.Iterator;
 import java.util.List;
 import org.idp.server.basic.json.JsonReadable;
 
-public class InputDescriptorConstrains
-    implements Iterable<InputDescriptorConstrainsFields>, JsonReadable {
+public class InputDescriptorConstrains implements Iterable<InputDescriptorConstrainsFields>, JsonReadable {
   List<InputDescriptorConstrainsFields> fields;
 
   public InputDescriptorConstrains() {
