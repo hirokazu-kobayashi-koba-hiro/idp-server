070c52065053a10906f010c5b3990777f4fd961c
hirokazu.kobayashi <hirokazu.kobayashi.koba.hiro@gmail.com>
2025-07-18 13:28:00 +0900

    add health api

---
+++ libs/idp-server-springboot-adapter/src/main/java/org/idp/server/adapters/springboot/HealthApi.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2025 Hirokazu Kobayashi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.idp.server.adapters.springboot;
+
+import org.idp.server.IdpServerApplication;
+import org.idp.server.adapters.springboot.application.restapi.ParameterTransformable;
+import org.idp.server.core.oidc.userinfo.UserinfoApi;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/health")
+public class HealthApi implements ParameterTransformable {
+
+  UserinfoApi userinfoApi;
+
+  public HealthApi(IdpServerApplication idpServerApplication) {
+    this.userinfoApi = idpServerApplication.userinfoApi();
+  }
+
+  @GetMapping
+  public ResponseEntity<?> get() {
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+}
